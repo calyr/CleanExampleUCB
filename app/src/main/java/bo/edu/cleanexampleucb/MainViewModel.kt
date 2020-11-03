99143d81d5f55fe633e2479cc4d64f04ec57a6b3
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2020-11-03 18:12:19 -0400

    added MainViewModel

--- app/src/main/java/bo/edu/cleanexampleucb/MainViewModel.kt
+++ app/src/main/java/bo/edu/cleanexampleucb/MainViewModel.kt
@@ -1,4 +1,29 @@
 package bo.edu.cleanexampleucb
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import bo.edu.domain.Movie
+import bo.edu.usecases.GetPopularMovie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val popularMovie: GetPopularMovie): ViewModel() {
+
+    val model: LiveData<UiModel>
+        get() = _model
+    private val _model = MutableLiveData<UiModel>()
+    sealed class UiModel {
+        class Content(val movies: List<Movie>): UiModel()
+    }
+
+    fun loadMovies() {
+        GlobalScope.launch(Dispatchers.IO) {
+            _model.value = UiModel.Content( popularMovie.invoke())
+        }
+
+    }
+
+
 }
